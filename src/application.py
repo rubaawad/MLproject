fa91c5352009259d87c0437c2c5b46ef5e7451da
Ruba Mahgoob <rubabdalla44@gmail.com>
2024-02-27 12:10:38 +0000

    Deployment configuration

--- src/application.py
+++ src/application.py
@@ -47,7 +47,7 @@ def predict_datapoint():
         return render_template('index.html', prediction=prediction_text)
     
 if __name__=="__main__":
-    app.run(debug=True,host="0.0.0.0",port=5000)
+    app.run(host="0.0.0.0",port=5000)
 # if __name__=="__main__":
 #     app.run(debug=True)
 #     app.run(host="0.0.0.0")        
